602d0ce73e1fe1643facb913a2265f5a2e08b4a7
KatyaSadykova <ThisIsKaterin@yandex.ru>
2024-12-02 20:30:00 +0300

    sprint_6

--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -3,6 +3,7 @@ package com.example;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
@@ -20,6 +21,7 @@ public class LionTest {
     @Test
     public void getKittensWithoutArgumentReturnOneTest() throws Exception {
         lion = new Lion(feline, "Самка");
+        Mockito.when(feline.getKittens()).thenReturn(1);
         int defaultKittensValue = 1;
 
         assertEquals(defaultKittensValue, lion.getKittens());
@@ -27,16 +29,18 @@ public class LionTest {
 
     @Test
     public void lionConstructorWrongSexReturnErrorTest() {
-        Exception exception = assertThrows(Exception.class, () -> lion = new Lion(feline, "Гермафродит"));
+        Exception exception = assertThrows(Exception.class, () -> {
+            lion = new Lion(feline, "Гермафродит");
+        });
 
-        assertEquals("Используйте допустимые значения пола животного - самей или самка", exception.getMessage());
+        assertEquals("Используйте допустимые значения пола животного - самец или самка", exception.getMessage());
     }
 
     @Test
     public void getFoodReturnPredatorFoodTest() throws Exception {
         lion = new Lion(feline, "Самка");
         List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
-
+        Mockito.when(feline.getFood("Хищник")).thenReturn(expectedFood);
 
         // Предполагается, что getFood принимает аргумент
         assertEquals(expectedFood, lion.getFood());
